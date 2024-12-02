1e0272efd0209c8a76e45e76ab72692daeba1819
Will Schlitzer <schlitzer90@gmail.com>
2024-12-02 14:26:02 -0500

    Create Day 3 blank template

---
+++ day3/day3_p1.py
@@ -0,0 +1,14 @@
+import time
+from day3_inputs import p1_sample, p1_puzzle
+start_time = time.time()
+
+
+def main():
+    safe_count = 0
+    rows_split = p1_puzzle.strip().split("\n")
+    #for row in rows_split:
+
+
+
+main()
+print(time.time() - start_time)
