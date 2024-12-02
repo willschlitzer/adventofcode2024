30c3890a7aae2317ead4a506b9062c309b293061
Will Schlitzer <schlitzer90@gmail.com>
2024-12-02 08:44:02 -0500

    Finish Day 1 Part 1

---
+++ day1/day1_p1.py
@@ -0,0 +1,32 @@
+from day1_inputs import p1_sample, p1_puzzle
+
+
+def main():
+    left_list = []
+    right_list = []
+    entry_difference = 0
+
+    input_split_rows = p1_puzzle.strip().split("\n")
+
+    for row in input_split_rows:
+        left, right = row_splitter(row)
+        left_list.append(left)
+        right_list.append(right)
+
+        left_list.sort()
+        right_list.sort()
+
+    for i in range(len(left_list)):
+        diff = abs(left_list[i] - right_list[i])
+        entry_difference += diff
+
+    print(entry_difference)
+
+
+def row_splitter(row):
+    left = int(row.split()[0])
+    right = int(row.split()[1])
+    return left, right
+
+
+main()
