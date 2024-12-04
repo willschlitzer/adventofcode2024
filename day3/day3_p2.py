03b010fcd0e5cefec8c49f4c8bae3a74ba37103e
Will Schlitzer <schlitzer90@gmail.com>
2024-12-04 00:51:24 -0500

    Finish Day 4 Part 1

--- day3/day3_p2.py
+++ day3/day3_p2.py
@@ -6,17 +6,28 @@ start_time = time.time()
 
 def main():
     data_input = p1_puzzle
-  #  data_input = p2_sample
-    dont_split = data_input.split("don't()")
-    math_string = dont_split[0]
-    do_list = [math_string]
-    for x in dont_split[1:]:
-        y = x.split("do()")
-        do_list.append(y[1])
-        print(do_list)
+   # data_input = p2_sample
+    find_pattern(input_string=data_input)
+    dont_pattern = r"don\'t\(\)"
+    dont_matches = re.findall(dont_pattern, data_input)
+    for _ in range(len(dont_matches)):
+        data_input = remove_dont_func(input_string=data_input)
+    #print(data_input)
+    dont_matches = re.findall(dont_pattern, data_input)
+    #find_multis(data_input=data_input)
+    find_pattern(input_string=data_input)
+
+def find_pattern(input_string):
+    do_pattern = r"(do\(\))(.+?)(don\'t\(\))"
+    do_matches = re.findall(do_pattern, input_string)
+    do_match_list = [x for x in do_matches]
+    for i in do_match_list:
+        print(i)
     
-    final_string = "".join(do_list)
-    find_multis(data_input=final_string)
+def remove_dont_func(input_string):
+    remove_pattern = r"(don\'t\(\))(.+?)(do\(\)|$)"
+    return re.sub(pattern=remove_pattern, repl="_",string=input_string)
+
 
 def find_multis(data_input):
     total_count = 0
---
+++ day4/day4_p1.py
@@ -0,0 +1,64 @@
+import time
+from day4_inputs import p1_sample, p1_puzzle
+import re
+import numpy as np
+start_time = time.time()
+
+
+def main():
+    data_input = p1_puzzle
+    data_array = data_input.strip().split("\n")
+    full_data_array = []
+    for entry in data_array:
+        row_list = []
+        for char in entry:
+            row_list.append(char)
+        full_data_array.append(row_list)
+    full_data_np = np.array(full_data_array)
+    full_data_np_tp = np.transpose(full_data_np)
+    search_words = ["XMAS", "SAMX"]
+    xmas_count = 0
+    for word in search_words:
+        xmas_count += word_search(word=word, array=full_data_np)
+        xmas_count += diagonal_word_search(word=word, array=full_data_np)
+        
+        xmas_count += word_search(word=word, array=full_data_np_tp)
+
+    print(xmas_count)
+
+
+def word_search(word, array):
+    single_xmas_count = 0
+    for row in array:
+        for i,char in enumerate(row[:-3]):
+            if char == word[0]:
+                if row[i+1] == word[1]:
+                    if row[i+2] == word[2]:
+                        if row[i+3] == word[3]:
+                            single_xmas_count += 1
+    return single_xmas_count
+
+def diagonal_word_search(word, array):
+    single_xmas_count = 0
+    for j,row in enumerate(array[:-3]):
+        for i,char in enumerate(row):
+            if char == word[0]:
+                if i >= 3:
+                    if array[j+1][i-1] == word[1]:
+                        if array[j+2][i-2] == word[2]:
+                            if array[j+3][i-3] == word[3]:
+                                print(array[j+3][i-3])
+                                single_xmas_count += 1
+                if i < len(row)-3:
+                    if array[j+1][i+1] == word[1]:
+                        if array[j+2][i+2] == word[2]:
+                            if array[j+3][i+3] == word[3]:
+                                single_xmas_count += 1
+    return single_xmas_count
+
+
+
+
+
+main()
+#print(time.time() - start_time)
