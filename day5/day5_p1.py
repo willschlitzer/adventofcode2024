b29ccd8f82c151804127ce762e1884d4763c0321
Will Schlitzer <schlitzer90@gmail.com>
2024-12-05 00:39:53 -0500

    Complete Day 5

---
+++ day5/day5_p1.py
@@ -0,0 +1,61 @@
+import time
+from day5_inputs import p1_sample_rules, p1_sample_lists, p1_puzzle_rules, p1_puzzle_lists
+
+start_time = time.time()
+
+
+def main():
+    rules = p1_puzzle_rules
+    updates = p1_puzzle_lists
+    rules_list = get_rules_list(rules=rules)
+    update_list = get_update_list(updates=updates)
+    good_updates = []
+    for update in update_list:
+        if check_page(update=update, rule_list=rules_list):
+            good_updates.append(update)
+    middle_num_sum = 0
+    for good_update in good_updates:
+        middle_index = int(len(good_update)/2)
+        middle_value = good_update[middle_index]
+        middle_num_sum += middle_value
+
+    print(middle_num_sum)
+
+
+def get_rules_list(rules):
+    rules_list = []
+    for rule in rules.strip().split("\n"):
+        temp_rule = rule.split("|")
+        temp_rule_list = []
+        for rule_str in temp_rule:
+            temp_rule_list.append(int(rule_str))
+        rules_list.append(temp_rule_list)
+    return rules_list
+
+def get_update_list(updates):
+    update_list = []
+    for update in updates.strip().split("\n"):
+        temp_update = update.split(",")
+        temp_update_list = []
+        for update_str in temp_update:
+            temp_update_list.append(int(update_str))
+        update_list.append(temp_update_list)
+    return update_list
+
+def check_page(rule_list, update):
+    for rule in rule_list:
+        x = rule[0]
+        y = rule[1]
+        for iy,ychar in enumerate(update):
+            if ychar == y:
+                for ix,xchar in enumerate(update):
+                    if xchar == x:
+                        if iy < ix:
+                            return False
+    return True
+
+
+
+
+main()
+# print(time.time() - start_time)
